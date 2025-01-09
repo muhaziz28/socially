9d4ece2b2ed1b3ed92bfa6817bce8aca9cad3431
Muhammad Haikal Aziz <kaal@Muhammads-MacBook-Pro.local>
2025-01-09 10:39:23 +0700

    fix sidebar ui

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -33,3 +33,20 @@ export async function syncUser() {
     console.log("Error in sync user ", error);
   }
 }
+
+export async function getUserByClerkId(clerkId: string) {
+  return await prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        },
+      },
+    },
+  });
+}
